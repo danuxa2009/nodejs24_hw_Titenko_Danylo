7d3c0b4b35c04e4765bcd0f6a8a64ee5938043b4
evhenious <evhenious@gmail.com>
2024-03-01 23:21:17 +0200

    small renaming

--- utils/usersService_sqlite.js
+++ utils/usersService_sqlite.js
@@ -2,19 +2,19 @@ const knexLib = require("knex");
 const knexConfig = require("../knexfile");
 
 const knex = knexLib(knexConfig);
-const USERS = "USERS";
+const USERS_TABLE = "USERS";
 
 const getAllUsers = async () => {
-  return await knex.select().from(USERS).returning("*");
+  return await knex.select().from(USERS_TABLE).returning("*");
 };
 const saveUsersToDB = async (data) => {
-  return await knex(USERS).insert(data);
+  return await knex(USERS_TABLE).insert(data);
 };
 const getUserById = async (id) => {
-  return await knex.select().from(USERS).where({ id });
+  return await knex.select().from(USERS_TABLE).where({ id });
 };
 const deleteUserById = async (id) => {
-  return await knex.delete().from(USERS).where({ id });
+  return await knex.delete().from(USERS_TABLE).where({ id });
 };
 
 module.exports = {
